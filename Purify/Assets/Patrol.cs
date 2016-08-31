09bbd5f3aa9fe34ed2fba1015de3551351a90024
asbrown1 <aalleexxbbrroowwnn@gmail.com>
2016-08-31 12:12:35 +1000

    Basic Enemy AI Works
    Created an enemy AI that can patrol to any amount of waypoints. Also
    added field of vision script that checks if friendly AI can see
    enemies/enemies can see friendly AI. At this point, only log indicates
    if players are seen.

---
+++ Purify/Assets/Patrol.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+    public Vector3 [] waypoints;
+    int currentWaypoint =0;
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 target = waypoints[currentWaypoint];
+        if(target.x==transform.position.x&& target.z == transform.position.z)
+        {
+            if (waypoints.Length > currentWaypoint + 1)
+            {
+                currentWaypoint++;
+            }
+            else
+                currentWaypoint = 0;
+        }
+        agent.destination = target;
+    }
+}
---
+++ Purify/Assets/SeePlayerCheck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeePlayerCheck : MonoBehaviour {
+    public GameObject[] targets;
+    public float fieldOfView;
+    public float visionRange;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*Based on user MattVic's sloution at http://answers.unity3d.com/questions/15735/field-of-view-using-raycasting.html*/
+	    for(int i=0;i<targets.Length;i++)
+        {
+            RaycastHit hit;
+            Vector3 rayDirection = targets[i].transform.position - this.transform.position;
+            if(Vector3.Angle(rayDirection,transform.forward)<fieldOfView)
+            {
+                if (Physics.Raycast(transform.position,rayDirection, out hit, visionRange))
+                {
+                    if(hit.transform.name==targets[i].transform.name)
+                    Debug.Log(this.gameObject.name + "can see " + hit.transform.name);
+                }
+            }
+        }
+	}
+}

664d452613842337f4c8fd3384f35f822f3ad096
asbrown1 <aalleexxbbrroowwnn@gmail.com>
2016-09-01 19:42:12 +1000

    Attack Phase
    Very basic attack phase working. AI will charge at each other and deal
    damage until one of them dies.

---
+++ Purify/Assets/Attack.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+    AIPhase phase;
+    SeePlayerCheck targetGet;
+    NavMeshAgent agent;
+    public int health=100;
+    public float rechargeTime=3.0f;
+    float timeLeft=0.0f;
+    public int attack = 5;
+    // Use this for initialization
+    void Start () {
+        phase = GetComponent<AIPhase>();
+        targetGet = GetComponent<SeePlayerCheck>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (this.gameObject.name != "Player")
+        {
+            if (phase.getPhase().Equals("Attack"))
+            {
+                Attack targetAttack;
+                GameObject target = GameObject.Find(targetGet.getTarget());
+                if (target)
+                {
+                    targetAttack = target.GetComponent<Attack>();
+                    if (Vector3.Distance(this.transform.position, target.transform.position) > 3)
+                        agent.destination = target.transform.position;
+                    else
+                    {
+                        agent.destination = this.transform.position;
+                        if (timeLeft <= 0)
+                        {
+                            targetAttack.reduceHealth(attack);
+                            timeLeft = rechargeTime;
+                        }
+                    }
+                }
+                else
+                {
+                    phase.setPhase("Follow");
+                }
+            }
+        }
+        if(timeLeft>0)
+        {
+            timeLeft = timeLeft-Time.deltaTime;
+        }
+        if (health <= 0)
+            Destroy(this.gameObject);
+    }
+    public void reduceHealth(int amount)
+    {
+        health = health - amount;
+        Debug.Log(gameObject.name + " now has " + health + " health");
+	}
+}
--- Purify/Assets/SeePlayerCheck.cs
+++ Purify/Assets/SeePlayerCheck.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SeePlayerCheck : MonoBehaviour {
     public GameObject[] targets;
     public float fieldOfView;
     public float visionRange;
     AIPhase phase;
+    String targetFound="";
 	// Use this for initialization
 	void Start () {
         phase = GetComponent<AIPhase>();
@@ -14,22 +16,36 @@ public class SeePlayerCheck : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         /*Based on user MattVic's sloution at http://answers.unity3d.com/questions/15735/field-of-view-using-raycasting.html*/
-	    for(int i=0;i<targets.Length;i++)
+        if (phase.getPhase().Equals("Follow") || phase.getPhase().Equals("Patrol"))
         {
-            RaycastHit hit;
-            Vector3 rayDirection = targets[i].transform.position - this.transform.position;
-            if(Vector3.Angle(rayDirection,transform.forward)<fieldOfView)
+            for (int i = 0; i < targets.Length; i++)
             {
-                if (Physics.Raycast(transform.position,rayDirection, out hit, visionRange))
+                RaycastHit hit;
+                Vector3 rayDirection;
+                if (targets[i])
                 {
-                    if (hit.transform.name == targets[i].transform.name)
+                    rayDirection = targets[i].transform.position - this.transform.position;
+                    if (Vector3.Angle(rayDirection, transform.forward) < fieldOfView)
                     {
-                        Debug.Log(this.gameObject.name + "can see " + hit.transform.name);
-                        if(!(phase.getPhase().Equals("Attack")))
-                            phase.setPhase("Attack");
+                        if (Physics.Raycast(transform.position, rayDirection, out hit, visionRange))
+                        {
+                            if (hit.transform.name == targets[i].transform.name)
+                            {
+                                Debug.Log(this.gameObject.name + "can see " + hit.transform.name);
+                                if (!(phase.getPhase().Equals("Attack")))
+                                {
+                                    phase.setPhase("Attack");
+                                    targetFound = hit.transform.name;
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
 	}
+    public string getTarget()
+    {
+        return targetFound;
+    }
 }

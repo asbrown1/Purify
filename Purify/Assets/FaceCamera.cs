7fca1631e36a710e91c5de25e9b0c0a1ba5eb524
asbrown1 <aalleexxbbrroowwnn@gmail.com>
2016-09-05 11:29:59 +1000

    Health Bars (WIP)
    Working on getting health bars over NPC's heads. Currently still not
    quite right and I'm not sure I'm even happy with the implementation
    (it's very messy)

---
+++ Purify/Assets/FaceCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Script from http://wiki.unity3d.com/index.php?title=CameraFacingBillboard. Makes objects directly face the camera
+
+public class FaceCamera : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+            Camera.main.transform.rotation * Vector3.up);
+    }
+}
--- Purify/Assets/SeePlayerCheck.cs
+++ Purify/Assets/SeePlayerCheck.cs
@@ -18,16 +18,16 @@ public class SeePlayerCheck : MonoBehaviour {
         /*Based on user MattVic's sloution at http://answers.unity3d.com/questions/15735/field-of-view-using-raycasting.html*/
         if (phase.getPhase().Equals("Follow") || phase.getPhase().Equals("Patrol"))
         {
-            for (int i = 0; i < targets.Length; i++)
+            for (int i = 0; i < targets.Length; i++)       //Checks for all targets
             {
-                RaycastHit hit;
-                Vector3 rayDirection;
-                if (targets[i])
+                RaycastHit hit;     //Ray Hit data
+                Vector3 rayDirection;   //Direction of ray
+                if (targets[i])     //If target still exists
                 {
                     rayDirection = targets[i].transform.position - this.transform.position;
-                    if (Vector3.Angle(rayDirection, transform.forward) < fieldOfView)
+                    if (Vector3.Angle(rayDirection, transform.forward) < fieldOfView)   //If character would be in field of view (without taking into account walls)
                     {
-                        if (Physics.Raycast(transform.position, rayDirection, out hit, visionRange))
+                        if (Physics.Raycast(transform.position, rayDirection, out hit, visionRange))    //Casts a ray in the direction of target to check for walls
                         {
                             if (hit.transform.name == targets[i].transform.name)
                             {
@@ -38,6 +38,15 @@ public class SeePlayerCheck : MonoBehaviour {
                                     targetFound = hit.transform.name;
                                 }
                             }
+                        }
+                    }
+                    if (Vector3.Magnitude(rayDirection)<2)      //If a target is close enough, it doesn't matter if they are in field of view
+                        {
+                        Debug.Log(this.gameObject.name + "can see " + targets[i].transform.name);
+                        if (!(phase.getPhase().Equals("Attack")))
+                        {
+                            phase.setPhase("Attack");
+                            targetFound = targets[i].transform.name;
                         }
                     }
                 }

3f75b1fe66630df0b597182b79e3a61524743a30
asbrown1 <aalleexxbbrroowwnn@gmail.com>
2016-09-18 19:39:51 +1000

    Bug fixes
    Fixed an issue where ranged enemies would stand still if no-one could be
    targetted. Also fixed an issue where friendly AI would follow the priest
    if they were nearby.

--- Purify/Assets/Attack.cs
+++ Purify/Assets/Attack.cs
@@ -12,6 +12,7 @@ public class Attack : MonoBehaviour {
     public string attackType = "Melee";
     public GameObject bullet;
     public float bulletVelocity = 100.0f;
+    public bool detailedLog = false;
     // Use this for initialization
     void Start () {
         phase = GetComponent<AIPhase>();
@@ -26,32 +27,45 @@ public class Attack : MonoBehaviour {
             agent.angularSpeed = agent.speed * 120;
             if (phase.getPhase().Equals("Attack"))
             {
+                if(detailedLog)
+                    Debug.Log(this.gameObject.name + " is in the attack phase");
                 if (attackType.Equals("Melee"))
                 {
+                    if (detailedLog)
+                        Debug.Log(this.gameObject.name + " is a melee type");
                     agent.speed = attackSpeed;
                     Health targetHealth;
                     GameObject target = GameObject.Find(targetGet.getTarget());
                     if (target)
                     {
+                        if (detailedLog)
+                            Debug.Log(this.gameObject.name + " is targetting "+target.gameObject.name);
                         targetHealth = target.GetComponent<Health>();
                         if (Vector3.Distance(this.transform.position, target.transform.position) > 3)
+                        {
                             agent.destination = target.transform.position;
+                            if (detailedLog)
+                                Debug.Log(this.gameObject.name + " is moving towards "+target.gameObject.name);
+                        }
                         else
                         {
+                            if (detailedLog)
+                                Debug.Log(this.gameObject.name + " is close enough to " + target.gameObject.name);
                             agent.destination = this.transform.position;
                             if (timeLeft <= 0)
                             {
-                                Debug.Log(gameObject.name);
+                                if (detailedLog)
+                                    Debug.Log(this.gameObject.name + " is attacking " + target.gameObject.name);
                                 targetHealth.reduceHealth(attack);
                                 if (target.name != "Player")
                                     setTargetToAttacker(target);
-                                Debug.Log(targetGet.name + " should attack " + gameObject.name);
                                 timeLeft = rechargeTime;
                             }
                         }
                     }
                     else
                     {
+                        Debug.Log("Taarget not found");
                         phase.setDefaultPhase();
                     }
                 }
@@ -76,12 +90,18 @@ public class Attack : MonoBehaviour {
                                     speedSet.setSpeed(rayDirection / rayDirection.magnitude * bulletVelocity);
                                     timeLeft = rechargeTime;
                                 }
-                            }
-                            else
-                            {
-                                phase.setDefaultPhase();
+                                else
+                                {
+                                    Debug.Log("Target not in range for " + this.gameObject.name + " Resume normal pattern");
+                                    phase.setDefaultPhase();
+                                }
                             }
                         }
+                    }
+                    else
+                    {
+                        Debug.Log("Target not found for " + this.gameObject.name);
+                        phase.setDefaultPhase();
                     }
 
                 }

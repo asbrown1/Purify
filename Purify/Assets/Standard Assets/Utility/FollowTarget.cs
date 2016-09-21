265fdf4189ca09dba89f28a29ac02db7692a5200
asbrown1 <aalleexxbbrroowwnn@gmail.com>
2016-09-21 16:59:45 +1000

    Changed Camera Controls
    Changed Camera controls so that they rotate by mouse (horizontally).
    ToDo: Make player move relative to camera.

--- Purify/Assets/Standard Assets/Utility/FollowTarget.cs
+++ Purify/Assets/Standard Assets/Utility/FollowTarget.cs
@@ -10,11 +10,15 @@ namespace UnityStandardAssets.Utility
         public Vector3 startOffset = new Vector3(0f, 7.5f, -4f);
         public float rotateSpeed = 1f;
 
+        void start()
+        {
+            transform.rotation = target.rotation;
+        }
         private void LateUpdate()
         {
             Vector3 offset = startOffset;
             Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed);
+            transform.Rotate(0,Input.GetAxis("Mouse X") * rotateSpeed,0,Space.World);
             offset.z = (float)Math.Cos(transform.eulerAngles.y*Mathf.Deg2Rad)*startOffset.z;
             offset.x = (float)Math.Sin(transform.eulerAngles.y*Mathf.Deg2Rad) * startOffset.z;
             transform.position = target.position + offset;

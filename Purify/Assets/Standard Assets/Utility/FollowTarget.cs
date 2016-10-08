a16e145ede8cff4679b63c58a1d105fc288511ba
asbrown1 <aalleexxbbrroowwnn@gmail.com>
2016-10-09 00:07:48 +1100

    Small Things
    Fixed the things that broke when I tried to resolve conflicts (basically
    Brendan's mana balance changes). Also fixed the starting camera rotation
    in code (a 'Start' function was missing a capital letter) and had a look
    at the cursor, but didn't get anything satisfying. Have an orange circle
    anyway.

--- Purify/Assets/Standard Assets/Utility/FollowTarget.cs
+++ Purify/Assets/Standard Assets/Utility/FollowTarget.cs
@@ -9,15 +9,16 @@ namespace UnityStandardAssets.Utility
         public Transform target;
         public Vector3 startOffset = new Vector3(0f, 7.5f, -4f);
         public float rotateSpeed = 1f;
+        Quaternion targetRotation;
 
-        void start()
+        void Start()
         {
-            transform.rotation = target.rotation;
+            targetRotation=Quaternion.Euler(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
+            transform.rotation = targetRotation;
         }
         private void LateUpdate()
         {
             Vector3 offset = startOffset;
-            Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
             transform.Rotate(0, Input.GetAxis("Mouse X")*Time.deltaTime*rotateSpeed, 0, Space.World);
             offset.z = (float)Math.Cos(transform.eulerAngles.y*Mathf.Deg2Rad)*startOffset.z;
             offset.x = (float)Math.Sin(transform.eulerAngles.y*Mathf.Deg2Rad) * startOffset.z;

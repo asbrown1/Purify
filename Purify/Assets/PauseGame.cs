b8039df74c52e6e9607e4d334c6f24b05448f849
radioactiv327 <brendanweeding@gmail.com>
2016-10-10 15:33:56 +1100

    Changes from earlier
    Added a skybox to level 1
    Added pause functionality to the player prefab, tests fine

---
+++ Purify/Assets/PauseGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour
+{
+
+    public bool isPaused = false; //check if the game is paused
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            togglePause();
+        }
+    }
+
+    void togglePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
